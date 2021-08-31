ab636287706e157db317152422142290d24c253d
antu0321 <anton.turtsau@netckracker.com>
2021-08-31 12:42:48 +0300

    add chapter3(Structure_Data).py

--- chapter3(Structure_Data).py
+++ chapter3(Structure_Data).py
@@ -38,15 +38,17 @@
 # print(fruits)
 
 
-vowels = ['a', 'e', 'i', 'o', 'u']
-word = input("Provide a word to search for vowels: ")
-found = {}
-for letter in word:
-    if letter in vowels:
-        found.setdefault(letter, 0)
-        found[letter] += 1
-for k, v in sorted(found.items()):
-    print(k, 'was found', v, 'item(s).')
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# word = input("Provide a word to search for vowels: ")
+# found = {}
+# for letter in word:
+#     if letter in vowels:
+#         found.setdefault(letter, 0)
+#         found[letter] += 1
+# for k, v in sorted(found.items()):
+#     print(k, 'was found', v, 'item(s).')
 
 
 ##### sets and tuples (Множества и кортежи)#####
+
+
