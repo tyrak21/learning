e4e5eb5889938fa8ec026730a9276a38524acb6a
antu0321 <anton.turtsau@netckracker.com>
2021-09-30 18:20:22 +0300

    ended part of chapter 4

--- CustomTasks.py
+++ CustomTasks.py
@@ -1,14 +1,32 @@
-s = "4епыапфц4ей3рпйшоизщи!2342#%!#%1353%!#%3%"
-string = ([s[i:i + 2] for i in range(0, len(s), 2)])
 
-def zadachka(string):
-    listof = []
-    for i in string:
-        if str(len(i)) == '2':
-            listof.append(i)
-        else:
-            listof.append(i + '_')
-    print(listof)
-zadachka(string)
+# match = "gjghdfgfrd".lower()
+# output = []
+# for i in str(match):
+#     removed = match.replace(i,"", 1)
+#     if i in removed:
+#         output.append('(')
+#     else:
+#         output.append(')')
+# full = ''.join(output)
+# print(full)
+
+
+# s = "4епыапфц4ей3рпйшоизщи!2342#%!#%1353%!#%3%"
+# string = ([s[i:i + 2] for i in range(0, len(s), 2)])
+#
+# def zadachka(string):
+#     listof = []
+#     for i in string:
+#         if str(len(i)) == '2':
+#             listof.append(i)
+#         else:
+#             listof.append(i + '_')
+#     print(listof)
+# zadachka(string)
+
+
+
+
+
 
 
--- chapter4(Function and modules).py
+++ chapter4(Function and modules).py
@@ -1,5 +1,5 @@
 #
-#
+# word = "fffffffoooo"
 # def search4vowels(word):
 #     """This is a comment in a functions"""
 #     vowels = set('aeiou')
@@ -7,8 +7,50 @@
 #     for vowel in found:
 #         print(vowel)
 #
-# search4vowels()
+# search4vowels(word)
 
 
 
+# word = 'fgha'
+# def search4vowels(word):
+#     """This is a comment in a functions"""
+#     vowels = set('aeiou')
+#     print(bool (vowels.intersection(set(word))))
+#
+# search4vowels(word)
+
+
+
+# word = 'fgh'
+# def search4vowels(word:str)->set:
+#     """returns the vowels found in the specified word"""
+#     vowels = set('aeiou')
+#     print(bool (vowels.intersection(set(word))))
+#
+# search4vowels(word)
+
+
+# phrase = 'fgh'
+# def search4vowels(phrase:str)->set:
+#     """returns the vowels found in the specified word"""
+#     vowels = set('aeiou')
+#     print(bool (vowels.intersection(set(phrase))))
+#
+# search4vowels(phrase)
+
+
+
+# def search4letters(phrase:str, letters:str)->set:
+#     """returns the vowels found in the specified word"""
+#     return set(letters).intersection(set(phrase))
+#
+# print(search4letters('abcde', 'abdefg'))
+
+
+
+def search4letters(phrase:str, letters:str='aeiou')->set:
+    """returns the vowels found in the specified word"""
+    return set(letters).intersection(set(phrase))
+
+print(search4letters('abcde'))
 
