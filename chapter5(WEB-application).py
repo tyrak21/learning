9563c3f7673bbb71ac745e98fb7700942266e772
antu0321 <anton.turtsau@netckracker.com>
2021-11-17 16:35:48 +0300

    added chapter 5 web application

--- chapter5(WEB-application).py
+++ chapter5(WEB-application).py
@@ -1,9 +1,27 @@
-from flask import Flask
+# from flask import Flask
+#
+# app= Flask(__name__)
+#
+# @app.route('/')
+# def hello() -> str:
+#
+#     return 'Hello world!'
+# app.run()
 
+
+
+from flask import Flask
+from vsearch import search4letters
 app= Flask(__name__)
 
 @app.route('/')
 def hello() -> str:
 
     return 'Hello world!'
+
+@app.route('/sear4')
+def do_search():
+    return str(search4letters('life, the universe, and everythin!')) ## this function was export to system
 app.run()
+
+
