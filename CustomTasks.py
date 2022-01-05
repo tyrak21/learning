5ae74e510c4bb25a0a36db48c64c5f12ef1711bc
antu0321 <anton.turtsau@netckracker.com>
2022-01-05 19:16:30 +0300

    codewars tasks

--- CustomTasks.py
+++ CustomTasks.py
@@ -24,9 +24,38 @@
 #     print(listof)
 # zadachka(string)
 
+# def multiply(a, b):
+#     result = a * b
+#     return result
+# print(multiply(4, 5))
 
 
+# def string_to_array(s):
+#     array = s.split(' ')
+#     return array
+# print(string_to_array(""))
 
 
-
+def rps(p1, p2):
+     if p1 == p2:
+         win = 0
+     if p1 == 'scissors' and p2 == 'rock':
+         win = 2
+     if p1 == 'paper' and p2 == 'rock':
+         win = 1
+     if p1 == 'rock' and p2 == 'paper':
+         win = 2
+     if p1 == 'scissors' and p2 == 'paper':
+         win = 1
+     if p1 == 'paper' and p2 == 'scissor':
+         win = 2
+     if p1 == 'rock' and p2 == 'scissors':
+         win = 2
+     if win == 0:
+         return "Draw!"
+     if win == 1:
+         return "Player 1 won!"
+     if win == 2:
+         return "Player 2 won!"
+print(rps('rock', 'scissor'))
 
